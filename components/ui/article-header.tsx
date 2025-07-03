2f2f341abb6ebb0e8cc2326d1000016dfbc33862
Eric Hasegawa <erichasegawa1@gmail.com>
2025-07-03 17:45:00 -0400

    add date to blog article

---
+++ components/ui/article-header.tsx
@@ -0,0 +1,23 @@
+import { sub } from 'motion/react-client';
+import React from 'react';
+
+interface ArticleHeaderProps {
+  title: string;
+  publishedDate: string;
+}
+
+export const ArticleHeader = ({
+    title,
+    publishedDate,
+    }: ArticleHeaderProps) => {
+    return (
+        <header>
+            <h1>
+                {title}
+            </h1>
+            <p className="text-sm -mt-4 italic">
+                {publishedDate}
+            </p>
+        </header>
+    );
+    }
